f24859e1c93878f7d3059afd5ed87c6159c2a98e
Valerian Backeberg <valerian@backebergs.de>
2021-10-01 22:05:26 +0200

    Add empty assignments component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AssignmentsComponent } from "./assignments/assignments.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "assignments",
+    pathMatch: "full",
+    component: AssignmentsComponent,
+  },
+  { path: "**", redirectTo: "/assignments" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
