43573c2e44e668517aa2880aed49eb6ec7317aca
Valerian Backeberg <valerian@backebergs.de>
2021-10-20 00:34:38 +0200

    Assignments component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AssignmentsComponent } from './assignments/assignments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AssignmentsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Assignment } from "../services/data/assignments";
+import { HttpService } from "../services/http.service";
 
 @Component({
-  selector: 'app-assignments',
-  templateUrl: './assignments.component.html',
-  styleUrls: ['./assignments.component.scss']
+  selector: "app-assignments",
+  templateUrl: "./assignments.component.html",
+  styleUrls: ["./assignments.component.scss"],
 })
 export class AssignmentsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  loading = true;
+  assignments: Assignment[] = [];
+  async ngOnInit(): Promise<void> {
+    this.assignments = await HttpService.getAssignments();
+    this.loading = false;
   }
-
 }
