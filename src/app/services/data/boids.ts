1663f72bfdfec6eb02f749208b320e33c70c455b
Valerian Backeberg <valerian@backebergs.de>
2021-10-20 00:33:16 +0200

    example boids

---
+++ src/app/services/data/boids.ts
@@ -0,0 +1,207 @@
+export interface Boid {
+  firstName: string;
+  lastName: string;
+  id: number;
+}
+export const returnedBoids: Boid[] = [
+  {
+    firstName: "Babe",
+    lastName: "Bennett",
+    id: 0,
+  },
+  {
+    firstName: "Balthazar",
+    lastName: "Blake",
+    id: 1,
+  },
+  {
+    firstName: "Benjamin",
+    lastName: "Button",
+    id: 2,
+  },
+  {
+    firstName: "Betsy",
+    lastName: "Bell",
+    id: 3,
+  },
+  {
+    firstName: "Billy",
+    lastName: "Brown",
+    id: 4,
+  },
+  {
+    firstName: "Bobby",
+    lastName: "Boucher",
+    id: 5,
+  },
+  {
+    firstName: "Chris",
+    lastName: "Cole",
+    id: 6,
+  },
+  {
+    firstName: "Clara",
+    lastName: "Clayton",
+    id: 7,
+  },
+  {
+    firstName: "Dale",
+    lastName: "Denton",
+    id: 8,
+  },
+  {
+    firstName: "Dawn",
+    lastName: "Davenport",
+    id: 9,
+  },
+  {
+    firstName: "Donnie",
+    lastName: "Darko",
+    id: 10,
+  },
+  {
+    firstName: "Donny",
+    lastName: "Donowitz",
+    id: 11,
+  },
+  {
+    firstName: "Francine",
+    lastName: "Fishpaw",
+    id: 12,
+  },
+  {
+    firstName: "Grant",
+    lastName: "Grant",
+    id: 13,
+  },
+  {
+    firstName: "Heather",
+    lastName: "Holloway",
+    id: 14,
+  },
+  {
+    firstName: "Hubert",
+    lastName: "Hawkins",
+    id: 15,
+  },
+  {
+    firstName: "James",
+    lastName: "Johnston",
+    id: 16,
+  },
+  {
+    firstName: "Jared",
+    lastName: "James",
+    id: 17,
+  },
+  {
+    firstName: "Jenny",
+    lastName: "Johnson",
+    id: 18,
+  },
+  {
+    firstName: "Leta",
+    lastName: "Lestrange",
+    id: 19,
+  },
+  {
+    firstName: "Linda",
+    lastName: "Litske",
+    id: 20,
+  },
+  {
+    firstName: "Lorne",
+    lastName: "Lutch",
+    id: 21,
+  },
+  {
+    firstName: "Marty",
+    lastName: "McFly",
+    id: 22,
+  },
+  {
+    firstName: "Nemo",
+    lastName: "Nobody",
+    id: 23,
+  },
+  {
+    firstName: "Nick",
+    lastName: "Naylor",
+    id: 24,
+  },
+  {
+    firstName: "Penny",
+    lastName: "Priddy",
+    id: 25,
+  },
+  {
+    firstName: "Ray",
+    lastName: "Reed",
+    id: 26,
+  },
+  {
+    firstName: "Richard",
+    lastName: "Riddick",
+    id: 27,
+  },
+  {
+    firstName: "Saul",
+    lastName: "Silver",
+    id: 28,
+  },
+  {
+    firstName: "Steve",
+    lastName: "Stifler",
+    id: 29,
+  },
+  {
+    firstName: "Sylvia",
+    lastName: "Stickles",
+    id: 30,
+  },
+  {
+    firstName: "Tony",
+    lastName: "Taira",
+    id: 31,
+  },
+  {
+    firstName: "Tracy",
+    lastName: "Turnbald",
+    id: 32,
+  },
+  {
+    firstName: "Veronica",
+    lastName: "Vaughn",
+    id: 33,
+  },
+  {
+    firstName: "Vic",
+    lastName: "Vega",
+    id: 34,
+  },
+  {
+    firstName: "Vicki",
+    lastName: "Vallencourt",
+    id: 35,
+  },
+  {
+    firstName: "Vincent",
+    lastName: "Vaga",
+    id: 36,
+  },
+  {
+    firstName: "Virginia",
+    lastName: "Venit",
+    id: 37,
+  },
+  {
+    firstName: "Wade",
+    lastName: "Walker",
+    id: 38,
+  },
+  {
+    firstName: "Wicket",
+    lastName: "Wystri",
+    id: 39,
+  },
+];
