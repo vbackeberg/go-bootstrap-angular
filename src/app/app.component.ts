65c52fb7fcacac4b7c37599f6b84581594e22524
Valerian Backeberg <valerian@backebergs.de>
2021-10-21 22:09:57 +0200

    initialize modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component } from "@angular/core";
+import { Modal } from "bootstrap";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'go-bootstrap-angular';
+export class AppComponent implements AfterViewChecked {
+  title = "go-bootstrap-angular";
+
+  ngAfterViewChecked() {
+    document
+      .querySelectorAll('[data-bs-toggle="modal"]')
+      .forEach((modalTrigger) => {
+        Modal.getOrCreateInstance(modalTrigger);
+      });
+  }
 }
